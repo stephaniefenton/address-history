d2d0fbd08259b126ea0848617ab6e38394e7bb6b
Stephanie Fenton <sfenton@vampadmins-MacBook-Pro.local>
2019-10-17 06:40:41 -0400

    Remove errant comment

--- src/api/index.js
+++ src/api/index.js
@@ -6,18 +6,6 @@ const axiosInstance = axios.create({
     baseURL: rootUrl,
 });
 
-// export const makeData = () => axiosInstance.post(
-//     '/addresses',
-//     {
-//         "user_id": "da9196a7-11bc-4b54-98c0-53699dbea942",
-//         "street_one": "13 Itte Road",
-//         "city": "Raleigh",
-//         "state": "NC",
-//         "zip_code": "27601"
-//     }
-// )
-
-// axiosInstance.post('/addresses/ea8630f0-eef2-11e9-92cc-51edf7fd79ed/restore')
 export const getAddresses = (userId) => axiosInstance.get(
     `/users/${userId}/addresses`,
 );
